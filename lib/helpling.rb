1a1a44282858f432afef05d91e471a91b5c09229
Tate Johnson <tate@tatey.com>
2014-11-26 20:53:00 +1000

    Split into rails and rack adapters.

--- lib/helpling.rb
+++ lib/helpling.rb
@@ -3,75 +3,126 @@ require 'helpling/version'
 require 'json'
 
 module Helpling
-  class Response < SimpleDelegator
-    def body
-      case __getobj__.content_type
-      when 'application/json'
-        JSON.parse(__getobj__.body)
-      else
-        __getobj__.body
+  module RackAdapter
+    class Response < SimpleDelegator
+      def body
+        case __getobj__.original_headers['CONTENT-TYPE']
+        when 'application/json'
+          JSON.parse(__getobj__.body)
+        else
+          __getobj__.body
+        end
+      end
+    end
+
+    class Request < AbstractAdapter::Request
+      def get_json(path, parameters, headers = {})
+        __getobj__.get(path, JSON.dump(parameters), @headers.merge(headers).merge('CONTENT-TYPE' => 'application/json'))
+      end
+
+      def post_json(path, parameters, headers = {})
+        __getobj__.post(path, JSON.dump(parameters), @headers.merge(headers).merge('CONTENT-TYPE' => 'application/json'))
+      end
+
+      def patch_json(path, parameters, headers = {})
+        __getobj__.patch(path, JSON.dump(parameters), @headers.merge(headers).merge('CONTENT-TYPE' => 'application/json'))
+      end
+
+      def put_json(path, parameters, headers = {})
+        __getobj__.put(path, JSON.dump(parameters), @headers.merge(headers).merge('CONTENT-TYPE' => 'application/json'))
+      end
+
+      def delete_json(path, parameters, headers = {})
+        __getobj__.delete(path, JSON.dump(parameters), @headers.merge(headers).merge('CONTENT-TYPE' => 'application/json'))
+      end
+
+      def response
+        Response.new(__getobj__.response)
       end
     end
   end
 
-  class Request < SimpleDelegator
-    def initialize(*args)
-      @headers = {}
-      super
+  module RailsAdapter
+    class Response < SimpleDelegator
+      def body
+        case __getobj__.content_type
+        when 'application/json'
+          JSON.parse(__getobj__.body)
+        else
+          __getobj__.body
+        end
+      end
     end
 
-    def header key, value
-      @headers[key] = value
-    end
+    class Request < AbstractAdapter::Request
+      def get_json(path, parameters, headers = {})
+        __getobj__.get(path, JSON.dump(parameters), @headers.merge(headers).merge('Content-Type' => 'application/json'))
+      end
 
-    def get(path, parameters = nil, headers = {})
-      __getobj__.get(path, parameters, @headers.merge(headers))
-    end
+      def post_json(path, parameters, headers = {})
+        __getobj__.post(path, JSON.dump(parameters), @headers.merge(headers).merge('Content-Type' => 'application/json'))
+      end
 
-    def post(path, parameters = nil, headers = {})
-      __getobj__.post(path, parameters, @headers.merge(headers))
-    end
+      def patch_json(path, parameters, headers = {})
+        __getobj__.patch(path, JSON.dump(parameters), @headers.merge(headers).merge('Content-Type' => 'application/json'))
+      end
 
-    def patch(path, parameters = nil, headers = {})
-      __getobj__.patch(path, parameters, @headers.merge(headers))
-    end
+      def put_json(path, parameters, headers = {})
+        __getobj__.put(path, JSON.dump(parameters), @headers.merge(headers).merge('Content-Type' => 'application/json'))
+      end
 
-    def put(path, parameters = nil, headers = {})
-      __getobj__.put(path, parameters, @headers.merge(headers))
-    end
+      def delete_json(path, parameters, headers = {})
+        __getobj__.delete(path, JSON.dump(parameters), @headers.merge(headers).merge('Content-Type' => 'application/json'))
+      end
 
-    def delete(path, parameters = nil, headers = {})
-      __getobj__.delete(path, parameters, @headers.merge(headers))
+      def response
+        Response.new(__getobj__.response)
+      end
     end
+  end
 
-    def get_json(path, parameters, headers = {})
-      __getobj__.get(path, JSON.dump(parameters), @headers.merge(headers).merge('Content-Type' => 'application/json'))
-    end
+  module AbstractAdapter
+    class Request < SimpleDelegator
+      def initialize(*args)
+        @headers = {}
+        super
+      end
 
-    def post_json(path, parameters, headers = {})
-      __getobj__.post(path, JSON.dump(parameters), @headers.merge(headers).merge('Content-Type' => 'application/json'))
-    end
+      def header key, value
+        @headers[key] = value
+      end
 
-    def patch_json(path, parameters, headers = {})
-      __getobj__.patch(path, JSON.dump(parameters), @headers.merge(headers).merge('Content-Type' => 'application/json'))
-    end
+      def get(path, parameters = nil, headers = {})
+        __getobj__.get(path, parameters, @headers.merge(headers))
+      end
 
-    def put_json(path, parameters, headers = {})
-      __getobj__.put(path, JSON.dump(parameters), @headers.merge(headers).merge('Content-Type' => 'application/json'))
-    end
+      def post(path, parameters = nil, headers = {})
+        __getobj__.post(path, parameters, @headers.merge(headers))
+      end
 
-    def delete_json(path, parameters, headers = {})
-      __getobj__.delete(path, JSON.dump(parameters), @headers.merge(headers).merge('Content-Type' => 'application/json'))
-    end
+      def patch(path, parameters = nil, headers = {})
+        __getobj__.patch(path, parameters, @headers.merge(headers))
+      end
 
-    def response
-      Response.new(__getobj__.response)
+      def put(path, parameters = nil, headers = {})
+        __getobj__.put(path, parameters, @headers.merge(headers))
+      end
+
+      def delete(path, parameters = nil, headers = {})
+        __getobj__.delete(path, parameters, @headers.merge(headers))
+      end
     end
   end
 
   module TestHelper
     def r
-      @r ||= Request.new(self)
+      @r ||= begin
+        if defined?(ActionDispatch::Integration::Runner) && self.class.ancestors.include?(ActionDispatch::Integration::Runner)
+          RailsAdapter::Request.new(self)
+        else
+          RackAdapter::Request.new(self)
+        end
+      end
     end
   end
 end
